39cc67dccb1c32f414374052d903e7e87bb0e66a
Rafi Septian <rafiseptian90@gmail.com>
2020-09-24 22:17:47 +0700

    news sectino

--- wp/wp-content/themes/GoGreeen/index.php
+++ wp/wp-content/themes/GoGreeen/index.php
@@ -28,7 +28,6 @@
             </div>
         </div>
     </section>
-
     <section id="solution">
         <div class="container">
             <div class="header text-center">
@@ -44,14 +43,45 @@
             <div class="content">
                 <div class="wrapper">
                     <img src="<?= get_template_directory_uri() . '/images/friendship.svg' ?>" alt="">
-                    <h3>Enjoy the </h3>
+                    <h3>the atmosphere around is cool</h3>
                 </div>
                 <div class="wrapper">
                     <img src="<?= get_template_directory_uri() . '/images/dreamer.svg' ?>" alt="">
+                    <h3>animals live peacefully in their habitat</h3>
                 </div>
                 <div class="wrapper">
                     <img src="<?= get_template_directory_uri() . '/images/reading_at_tree.svg' ?>" alt="">
+                    <h3>Enjoy the atmosphere</h3>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section id="news">
+        <div class="container">
+            <div class="header text-center">
+                <h3 class="section-header">
+                    News
+                </h3>
+                <span class="d-block">
+                    Let see the information about our work below
+                </span>
+            </div>
+
+            <div class="content">
+                <?php $data_news = get_posts([
+                        'post_type' => 'posts',
+                        'numberposts' => 3,
+                ]) ?>
+
+                <?php foreach($data_news as $news) : ?>
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title">
+
+                            </h3>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </section>
