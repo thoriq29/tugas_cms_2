3f13d3395a2c5ce3ff128c14345c2dfd6d0d0c70
Rafi Septian <rafiseptian90@gmail.com>
2020-09-24 22:02:46 +0700

    solution section

--- wp/wp-content/themes/GoGreeen/index.php
+++ wp/wp-content/themes/GoGreeen/index.php
@@ -28,6 +28,33 @@
             </div>
         </div>
     </section>
+
+    <section id="solution">
+        <div class="container">
+            <div class="header text-center">
+                <h3 class="section-header">
+                    What are the benefits
+                </h3>
+
+                <span class="d-block">
+                    we can get so many benefits like
+                </span>
+            </div>
+
+            <div class="content">
+                <div class="wrapper">
+                    <img src="<?= get_template_directory_uri() . '/images/friendship.svg' ?>" alt="">
+                    <h3>Enjoy the </h3>
+                </div>
+                <div class="wrapper">
+                    <img src="<?= get_template_directory_uri() . '/images/dreamer.svg' ?>" alt="">
+                </div>
+                <div class="wrapper">
+                    <img src="<?= get_template_directory_uri() . '/images/reading_at_tree.svg' ?>" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php get_footer(); ?>
