f43e91725ed442dc673bbfeac6e55bacd76ae6d8
Rafi Septian <rafiseptian90@gmail.com>
2020-09-24 22:50:15 +0700

    done footer

--- wp/wp-content/themes/GoGreeen/footer.php
+++ wp/wp-content/themes/GoGreeen/footer.php
@@ -1,13 +1,18 @@
 <footer id="footer">
     <div class="container">
-        <div class="subscribe">
+        <div class="invite">
+            <h3>Let us invite you if you are interested</h3>
             <form action="" method="post">
                 <input type="text" class="custom-input" placeholder="example@example.com">
+                <button type="submit" class="btn btn-primary">
+                    Invite me
+                </button>
             </form>
         </div>
 
         <div class="share">
-
+            <h3>Share our community</h3>
+            <?= do_shortcode('[DISPLAY_ULTIMATE_SOCIAL_ICONS] ') ?>
         </div>
     </div>
 </footer>
