a8e4a1b4e4e1a344155ed57a886bbf5e62df8e11
Rafi Septian <rafiseptian90@gmail.com>
2020-09-24 21:47:47 +0700

    done sympathy section

--- wp/wp-content/themes/GoGreeen/index.php
+++ wp/wp-content/themes/GoGreeen/index.php
@@ -1,7 +1,33 @@
 <?php get_header(); ?>
 
 <main id="main-content">
+    <section id="sympathy">
+        <div class="container">
+            <div class="header text-center">
+                <h3 class="section-header">
+                    Our motivation
+                </h3>
 
+                <span class="d-block">
+                    The following are a few examples of why we raise your concern
+                </span>
+            </div>
+            <div class="content">
+                <div class="img-wrapper">
+                    <img src="<?= get_template_directory_uri() . '/images/forest_fires.jpg' ?>" alt="">
+                </div>
+                <div class="img-wrapper">
+                    <img src="<?= get_template_directory_uri() . '/images/logging.jpg' ?>" alt="">
+                </div>
+                <div class="img-wrapper">
+                    <img src="<?= get_template_directory_uri() . '/images/smog.jpg' ?>" alt="">
+                </div>
+                <div class="img-wrapper">
+                    <img src="<?= get_template_directory_uri() . '/images/poor_animal.jpg' ?>" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php get_footer(); ?>
