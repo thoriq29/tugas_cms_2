b289734f3228ec62975fddd955081d75d8897b3c
Rafi Septian <rafiseptian90@gmail.com>
2020-09-25 10:00:39 +0700

    still_single_news

--- wp/wp-content/themes/GoGreeen/single-news.php
+++ wp/wp-content/themes/GoGreeen/single-news.php
@@ -5,18 +5,26 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>GoGreen</title>
+    <title>GoGreeen</title>
     <?php wp_head(); ?>
 </head>
 <body>
 
-<main id="detail-news">
+<section id="detail-news">
     <div class="container">
-        <div class="content">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        </div>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <div class="content">
+                <img src="<?= get_the_post_thumbnail() ?>
+                <h3 class="text-center">
+                    <?= the_title(); ?>
+                </h3>
+                <p>
+                    <?= the_content(); ?>
+                </p>
+            </div>
+        <?php endwhile; endif; ?>
     </div>
-</main>
+</section>
 
 <?php get_footer(); ?>
 </body>
